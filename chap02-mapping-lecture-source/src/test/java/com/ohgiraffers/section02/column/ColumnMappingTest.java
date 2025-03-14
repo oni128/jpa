3c075ed4c6ba5615a9fc11831c95dedf0fc5dae8
oni128 <lyw283828@gmail.com>
2025-03-14 09:50:56 +0900

    mapping 첫번째 예제 추가

---
+++ chap02-mapping-lecture-source/src/test/java/com/ohgiraffers/section02/column/ColumnMappingTest.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.section02.column;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+public class ColumnMappingTest {
+    private static EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+}
